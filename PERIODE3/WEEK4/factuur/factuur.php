728fef99fe5f92ec1c35d453fba72425ed199c60
Konyali4242 <uvys123@gmail.com>
2024-03-08 22:07:50 +0100

    name fix

--- PERIODE3/WEEK4/factuur/factuur.php
+++ PERIODE3/WEEK4/factuur/factuur.php
@@ -9,11 +9,7 @@ class Factuur {
         $this->dbh = $dbh;
     }
     public function insertFactuur ($F_Datum, $Btw6, $Btw19, $Totaal_excl_BTW, $Totaal_incl_BTW, $Prijs_totaal, $Aantal_producten, $Tafel_id, $Product_id){
-<<<<<<< HEAD
         return $this->dbh->execute("INSERT INTO factuur (F_Datum, '6%Btw', '19%Btw', Totaal_excl_BTW, Totaal_incl_BTW, Prijs_totaal, Aantal_producten, Tafel_id, Product_id) 
-=======
-        return $this->dbh->execute("INSERT INTO factuur (F_Datum, '6%Btw', '19%Btw', Totaal_excl_BTW, Totaal_incl_BTW, Prijs_totaal, aantal_producten, Tafel_id, Product_id) 
->>>>>>> 7c5c3556ad9e690eaa2c93a7766f0c44568d6896
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [$F_Datum, $Btw6, $Btw19, $Totaal_excl_BTW, $Totaal_incl_BTW, $Prijs_totaal, $Aantal_producten, $Tafel_id, $Product_id]);
     }
 }
